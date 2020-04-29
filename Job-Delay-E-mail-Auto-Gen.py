38ce0636c00c82f5d8731e2cd16b3ef222bd304f
Ballsnacks <64499484+Ballsnacks@users.noreply.github.com>
2020-04-29 19:25:02 -0400

    First

---
+++ Job-Delay-E-mail-Auto-Gen.py
@@ -0,0 +1,16 @@
+#Don't laugh
+#I know im stupid and probably did something wrong
+#but it works, ok?
+#I will give $100 to anyone that actually uses this to contact a customer
+corporation = input ("\n \nInsert Customer Name: ")
+shorthand_name = input ("\nInsert Customer Shorthand Name: ")
+job = input ("\nWhat are we late on this time: ")
+
+
+customer_name = corporation
+nick_name = shorthand_name
+job = job #look, i know what i did. Im not proud of it
+print(" \n \n _____________________________________________ \n Dear "+ nick_name +" \n we regret to inform you that your \n "+ job +" have been delayed, please contact our \n offices to find out more information. \n we hope this does not tarnish our \n upstanding relationship with "+ customer_name +"'s accounting department\n thank you for your patience "+ nick_name +"\n we look foward to dodging your emails in the future!\n \n \n P.S YOUR NEVER GETTING THESE PARTS! SUCKERS!!")
+print("_____________________________________________")
+
+#Advice is welcome!
